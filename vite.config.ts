c6a32fc6beb5f51ae89a65520a5f5db9394d1ef1
Pedro Aldo <pedroaldo2025@gmail.com>
2024-04-07 13:37:41 -0300

    Atualizações para o deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/net-project",
+  base: "/Netflix-Project",
 })
